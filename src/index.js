3444cc0c5d9622bdd175f0dd9378cd42e40d296a
Harold_Mesa <harold.mesa@icloud.com>
2022-09-03 19:05:53 -0500

    Format package.json. WIP: Working on UI

--- src/index.js
+++ src/index.js
@@ -9,14 +9,22 @@ function handleFormSubmission(event) {
   
   let userAge = parseInt(document.querySelector("input#ageInput").value);
   let lifeExpForUserOnEarth = parseInt(document.querySelector("input#lifeExpInput").value);
-  
+  let text1 = document.getElementById("text1");
 
   let calculator = new Calculator(userAge, lifeExpForUserOnEarth);
-  
+  let mercury = calculator.toMercuryYears();
+  let venus = calculator.toVenusYears();
+  let mars = calculator.toMarsYears();
+  let jupiter =calculator.toJupiterYears();
+
+  text1.innerHTML = ` If you lived in Mercury, you'd be ${mercury} y.o,<br> if you lived in Venus, ${venus} y.o,<br> ${mars} y.o if in Mars, and ${jupiter} y.o if you lived in Jupiter!`;
+
+
+  document.getElementById("results").removeAttribute("class", "hidden");
   return calculator;
 }
 
 window.addEventListener("load", function() {
-  let form = document.querySelector("form");
+  let form = document.querySelector("form#form1");
   form.addEventListener("submit", handleFormSubmission);
 });
